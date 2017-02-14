45c7944c2374a72ffd5f2231a7f3e6d0573ad637
Ryan778 <Ryan778@users.noreply.github.com>
2017-02-13 18:04:14 -0700

    Update script.user.js
    Add Dance Mat Typing to the whitelist

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Website Blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Blocks non-hw related sites
 // @author       Ryan
 // @match        http*://*/*
@@ -17,7 +17,10 @@
     'use strict';
     if(!!location.href.match(/http.*:\/\//)){
         var allowed = ['app.readingeggs.com', 'student.mathseeds.com', 'kidsa-z.com', 'learnersdictionary.com', 'www.learnersdictionary.com', '10.10.1.140', '50.155.208.17', 'www.kidsa-z.com', 'www.google.com'];
-        if(allowed.indexOf(location.hostname) === -1 || location.hostname === 'www.google.com' && location.href.indexOf('q=') !== -1){
+        var exception = false;
+        var title = (document.getElementsByTagName('title').length>0?document.getElementsByTagName('title')[0].innerHTML:'');
+        if(location.pathname === '/guides/z3c6tfr' && location.hostname === 'www.bbc.co.uk' || location.hostname === 'play.bbc.co.uk' && title.indexOf('Dance Mat Typing') !== -1){exception = true}
+        if(allowed.indexOf(location.hostname) === -1 && !exception || location.hostname === 'www.google.com' && location.href.indexOf('q=') !== -1){
             window.open('http://50.155.208.17:8081/riley/page-blocked/?goback=1','_self');
         }
     }
