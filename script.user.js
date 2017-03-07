90fde15d44a34723c7fde5bc602d7407ca49ad03
Ryan778 <Ryan778@users.noreply.github.com>
2017-03-07 16:39:52 -0700

    Update script.user.js to v0.4
    Add hash checking, target site tag, and whitelisted the Zach and PSD websites

--- script.user.js
+++ script.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Website Blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Blocks non-hw related sites
+// @version      0.4
+// @description  Blocks non-hw related sites (For Personal Use)
 // @author       Ryan
 // @match        http*://*/*
 // @grant        none
@@ -14,9 +14,24 @@
 // ==/UserScript==
 
 (function() {
+    function genCode(){
+        var d = new Date();
+        var a = Math.pow((d.getDate()*3),Math.abs((d.getDay()*2-12)));
+        a *= (d.getMonth()+3);
+        a += d.getYear();
+
+        var a1 = Math.round(a/(d.getDay()+1));
+        while (a1.toString().length <= 6){
+            a1 *= (Math.abs(Math.round((d.getDate()-14)/2))+4);
+            a1 += d.getFullYear();
+        }
+        a1 = a1.toString().substr(0,6);
+        return a1;
+    }
+    
     'use strict';
     if(!!location.href.match(/http.*:\/\//)){
-        var allowed = ['app.readingeggs.com', 'student.mathseeds.com', 'kidsa-z.com', 'learnersdictionary.com', 'www.learnersdictionary.com', '10.10.1.140', '50.155.208.17', 'www.kidsa-z.com', 'www.google.com'];
+        var allowed = ['app.readingeggs.com', 'student.mathseeds.com', 'kidsa-z.com', 'learnersdictionary.com', 'www.learnersdictionary.com', '10.10.1.140', '50.155.208.17', 'www.kidsa-z.com', 'www.google.com', 'zac.psdschools.org', 'www.psdschools.org', 'kin.psdschools.org'];
         var exception = false;
         var title = (document.getElementsByTagName('title').length>0?document.getElementsByTagName('title')[0].innerHTML:'');
         if(location.pathname === '/guides/z3c6tfr' && location.hostname === 'www.bbc.co.uk' || location.hostname === 'play.bbc.co.uk' && title.indexOf('Dance Mat Typing') !== -1){exception = true}
@@ -26,7 +41,15 @@
             else if(location.pathname === '/riley/yt/player.html'){blacklisted = true}
         }
         if(allowed.indexOf(location.hostname) === -1 && !exception || location.hostname === 'www.google.com' && location.href.indexOf('q=') !== -1 || blacklisted){
-            window.open('http://50.155.208.17:8081/riley/page-blocked/?goback=1','_self');
+            window.open('http://50.155.208.17:8081/riley/page-blocked/?goback=1&targetsite='+location.href,'_self');
+        }
+        var restrictedSites = ['10.10.1.140:8092','10.10.1.140:8097','10.10.1.140:8091'];
+        if(restrictedSites.indexOf(location.host) !== -1 && location.pathname.indexOf('api') === -1){
+            var hash = location.hash.slice(1);
+            if(hash !== genCode()){
+                alert('Invalid Access Code.\nIf you believe this is an error, please contact Ryan.');
+                window.open('http://50.155.208.17:8081/riley/page-blocked/?goback=1','_self');
+            }
         }
     }
 })();
